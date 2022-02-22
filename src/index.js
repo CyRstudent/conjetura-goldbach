24f202b1a6cfc380293c2eb65dde3033591882be
CyRstudent <a1433hsaez@iesmarmediterraneo.org>
2022-02-22 16:36:55 +0100

    start over

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-const esPrimo = require('./functions/esPrimo');
 const prompt = require('prompt-sync')();
 const StrAns = prompt('Introduce un numero entero que sea mayor que 2: ');
-if (typeof ans == 'undefined' || ans == null ) {
+if (typeof StrAns == 'undefined' || StrAns == null ) {
 	// eslint-disable-next-line max-len
 	console.log('Tienes que introducir algo primero. ¿Es tan dificil para ti leer?');
 	process.exit(0);
@@ -11,32 +10,4 @@ let n = Number(StrAns);
 if (isNaN(n) || n == null) {
 	console.log('Te he pedido que introduzcas un numero, payaso');
 	process.exit(0);
-}
-
-// El codigo de verdad empieza aqui, lo de arriba era solo para coger datos
-const arr = [];
-let i = 1;
-do {
-	for (let j = 1; j <= i; j++) {
-		const pBool = esPrimo(j);
-		const arrElement = arr.find(j);
-		if (pBool &&
-			typeof arrElement == 'undefined' ||
-			arrElement == null) {
-			arr.push(j);
-		}
-	}
-	i++;
-}
-while (arr[arr.length--] <= n);
-
-for (let i = 0; i < arr.length--; i++) {
-	for (let l = i++; l < arr.length--; l++) {
-		const primoA = arr[i];
-		const primoB = arr[l];
-		if (primoB + primoA == n) {
-			console.log(`${primoB} + ${primoA} = ${n}`);
-			process.exit();
-		}
-	}
 }
